499824cd5a91e8f936b1ae1635fe39af1ad7908f
sauravnr <sauravniroula02@gmail.com>
2025-07-29 20:54:31 +0545

    bet modal updated

--- src/components/BetModal.jsx
+++ src/components/BetModal.jsx
@@ -44,7 +44,7 @@ export default function BetModal({
               className="w-6 h-6 mx-auto"
             />
           </button>
-          <div className="mx-4 flex items-center justify-center w-20">
+          <div className="mx-4 flex items-center justify-center w-24 bg-[#eeebe3] border border-[#e0c08b] rounded-xl shadow-[0_2px_0_#c7994a,0_4px_2px_rgba(0,0,0,0.3)] py-1">
             <img src="/icons/coin.png" alt="coin" className="w-4 h-4 mr-1" />
             <span className="text-lg font-semibold">{current.entry}</span>
           </div>
@@ -56,7 +56,7 @@ export default function BetModal({
             />
           </button>
         </div>
-        <div className="flex items-center justify-center mt-2 ml-4">
+        <div className="flex items-center justify-center mt-3 ml-5">
           <div className="flex items-center mr-1 flex-shrink-0">
             <span className="text-sm mr-1">Win:</span>
             <img
